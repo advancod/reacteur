79b5dcd469f40233aa1504a42c1f019caca39e77
advancod <advancod92@gmail.com>
2019-08-23 15:41:39 +0200

    exemple overmind

--- projet2/src/App.js
+++ projet2/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as yup from 'yup'
+import { Formik } from 'formik'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component, Fragment } from 'react';
+import { TextInput, Text, Button, Alert } from 'react-native';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={values => Alert.alert(JSON.stringify(values))}
+        validationSchema={yup.object().shape({
+          email: yup
+            .string()
+            .email()
+            .required(),
+          password: yup
+            .string()
+            .min(6)
+            .required(),
+        })}
+      >
+        {({ values, handleChange, errors, setFieldTouched, touched, isValid, handleSubmit }) => (
+          <Fragment>
+            <TextInput
+              value={values.email}
+              onChangeText={handleChange('email')}
+              onBlur={() => setFieldTouched('email')}
+              placeholder="E-mail"
+            />
+            {touched.email && errors.email &&
+              <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
+            }
+            <TextInput
+              value={values.password}
+              onChangeText={handleChange('password')}
+              placeholder="Password"
+              onBlur={() => setFieldTouched('password')}
+              secureTextEntry={true}
+            />
+            {touched.password && errors.password &&
+              <Text style={{ fontSize: 10, color: 'red' }}>{errors.password}</Text>
+            }
+            <Button
+              title='Sign In'
+              disabled={!isValid}
+              onPress={handleSubmit}
+            />
+          </Fragment>
+        )}
+      </Formik>
+    );
+  }
 }
-
-export default App;
